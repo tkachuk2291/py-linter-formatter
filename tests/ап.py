a58b257dcee8fc5e62e2a736e99d6586dfeca6df
Anton Tkachuk <132574548+tkachuk2291@users.noreply.github.com>
2023-07-11 21:21:29 +0300

    Delete ап.py

--- tests/ап.py
+++
@@ -1,9 +0,0 @@
-def greet():
-    print("Hello, world!")
-
-def perform_action(greet):
-    print("Performing action...")
-    greet()
-
-# # Передача функции greet в функцию perform_action
-perform_action(greet)
